5b7e0875dbed5153a26b04316d0390959aa4cf3b
ErikRidhoFirm <erikridh1@gmail.com>
2024-11-13 12:57:47 +0700

    percobaan10

---
+++ minggu9/praktik_php/homeSession.php
@@ -0,0 +1,18 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+        session_start();
+        if ($_SESSION['status'] == 'login') {
+            echo "selamat datang " . $_SESSION['username'];
+            ?>
+            <br> <a href="sessionLogout.php">LogOut</a>
+            <?php
+        }else{
+            echo "anda belum login. silahkan "?>
+            <a href="sessionLoginForm.html">Log in</a>
+            <?php
+        }
+        ?>
+    </body>
+</html>
